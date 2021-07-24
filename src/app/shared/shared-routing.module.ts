fb0eb9f1990be8fdc5f4b8c30c66eb5c568501a5
marieme diop <mariemesembenediop@gmail.com>
2021-07-24 21:55:58 +0000

    integration

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from "@angular/router";
 import { EquipeComponent } from "./equipe/equipe.component";
 import { HelpComponent } from "./help/help.component";
 import { ListPublicationsComponent } from "./list-publications/list-publications.component";
+import { LoginComponent } from "./login/login.component";
 import { ProfilComponent } from "./profil/profil.component";
+import { SignupComponent } from "./signup/signup.component";
 
 const routes: Routes = [
     {
@@ -11,9 +13,18 @@ const routes: Routes = [
       redirectTo:'publications',
       pathMatch:'full'
     },
+    
     {
       path:'publications',
       component:ListPublicationsComponent,
+    },
+    {
+      path:'connexion',
+      component:LoginComponent,
+    },
+    {
+      path:'inscription',
+      component:SignupComponent,
     },
     {
       path:'profil',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,19 +5,24 @@ import { ProfilComponent } from './profil/profil.component';
 import { HelpComponent } from './help/help.component';
 import { RouterModule } from '@angular/router';
 import { EquipeComponent } from './equipe/equipe.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { CoreModule } from '../core/core.module';
 
 
 
 @NgModule({
-  declarations: [ListPublicationsComponent, ProfilComponent, HelpComponent, EquipeComponent],
+  declarations: [ListPublicationsComponent, ProfilComponent, HelpComponent, EquipeComponent, LoginComponent, SignupComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CoreModule
   ],
   exports: [
     ListPublicationsComponent,
     ProfilComponent,
     HelpComponent,
+    
   ]
 })
 export class SharedModule { }
